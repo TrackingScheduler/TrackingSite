702c5060527db0a03865ac937272154544e0b5c8
TrackingScheduler <101292138+TrackingScheduler@users.noreply.github.com>
2022-04-04 15:23:58 -0400

    before class 4-4

--- js/main.js
+++
@@ -1,6 +0,0 @@
-let feelLow = document.querySelector('#feelLow');
-let feelLowD = document.querySelector('#feelLowD');
-
-btnAdd.addEventListener('click', () => {
-    feelLowD.value = parseInt(feelLowD.value) + 1;
-});
